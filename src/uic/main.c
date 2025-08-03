7b77f626d8fd2ce1a6a00ff291f6624b11f9c817
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2025-08-03 15:28:07 -0400

    Add uic

---
+++ src/uic/main.c
@@ -0,0 +1,17 @@
+#include <libqt6c.h>
+#include "design.h"
+
+int main(int argc, char* argv[]) {
+    // Initialize Qt application
+    q_application_new(&argc, argv);
+
+    MainWindowUi* uic = new_main_window_ui();
+
+    q_mainwindow_show(uic->MainWindow);
+
+    int result = q_application_exec();
+
+    free(uic);
+
+    return result;
+}
