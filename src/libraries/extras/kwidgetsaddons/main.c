3e684741e1598a3a9a9c7a0dbd68d0bdd908e772
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2025-08-24 18:49:54 -0400

    Add kwidgetsaddons
    Ref: https://invent.kde.org/frameworks/kwidgetsaddons/-/blob/master/examples/kmessagebox/main.cpp

---
+++ src/libraries/extras/kwidgetsaddons/main.c
@@ -0,0 +1,20 @@
+#include <libqt6c.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    q_application_new(&argc, argv);
+
+    KGuiItem* primaryAction = k_guiitem_new7("Hello", "view-filter", "This is a tooltip", "This is a WhatsThis help text.");
+
+    KGuiItem* secondaryAction = k_guiitem_new2("Bye");
+
+    int32_t res = k_messagebox_question_two_actions(NULL, "Description to tell you to click<br />on <b>either</b> button", "Qt 6 KMessageBox Example", primaryAction, secondaryAction, "", KMESSAGEBOX_OPTION_NOTIFY);
+
+    if (res == KMESSAGEBOX_BUTTONCODE_PRIMARYACTION) {
+        printf("You clicked Hello\n");
+    } else {
+        printf("You clicked Bye\n");
+    }
+
+    return 0;
+}
