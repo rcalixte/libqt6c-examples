a9a67874f319f88a5663998f9ec09ee281dfb4d0
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2025-08-28 20:06:53 -0400

    Add sonnet
    Ref: https://invent.kde.org/frameworks/sonnet/-/blob/master/examples/textedit.cpp

---
+++ src/libraries/extras/sonnet/main.c
@@ -0,0 +1,46 @@
+#include <libqt6c.h>
+
+static Sonnet__Highlighter* highlighter1;
+static Sonnet__Highlighter* highlighter2;
+
+static void on_dictionary_changed(void* UNUSED self, const char* dictionary) {
+    k_sonnet__highlighter_set_current_language(highlighter1, dictionary);
+    k_sonnet__highlighter_set_current_language(highlighter2, dictionary);
+}
+
+int main(int argc, char* argv[]) {
+    q_application_new(&argc, argv);
+
+    QWidget* widget = q_widget_new2();
+    q_widget_set_window_title(widget, "Qt 6 Sonnet Example");
+
+    Sonnet__DictionaryComboBox* comboBox = k_sonnet__dictionarycombobox_new2();
+    QTextEdit* textedit1 = q_textedit_new2();
+    q_textedit_set_text(textedit1, "This is a sample buffer. Whih this thingg will be checkin for misstakes. Whih, Enviroment, covermant. Whih.");
+
+    Sonnet__SpellCheckDecorator* installer1 = k_sonnet__spellcheckdecorator_new(textedit1);
+    highlighter1 = k_sonnet__spellcheckdecorator_highlighter(installer1);
+
+    k_sonnet__highlighter_set_current_language(highlighter1, "en_US");
+
+    QTextEdit* textedit2 = q_textedit_new2();
+    q_textedit_set_text(textedit2, "John Doe said:\n> Hello how aree you?\nI am ffine thanks");
+
+    Sonnet__SpellCheckDecorator* installer2 = k_sonnet__spellcheckdecorator_new(textedit2);
+    highlighter2 = k_sonnet__spellcheckdecorator_highlighter(installer2);
+
+    k_sonnet__highlighter_set_current_language(highlighter2, "en_US");
+
+    k_sonnet__dictionarycombobox_on_dictionary_changed(comboBox, on_dictionary_changed);
+
+    QVBoxLayout* layout = q_vboxlayout_new(widget);
+    q_vboxlayout_add_widget(layout, comboBox);
+    q_vboxlayout_add_widget(layout, textedit1);
+    q_vboxlayout_add_widget(layout, textedit2);
+
+    q_widget_show(widget);
+
+    int ret = q_application_exec();
+
+    return ret;
+}
