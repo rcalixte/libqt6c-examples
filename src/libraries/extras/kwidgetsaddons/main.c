9f7c3ae2e2227a50653e61beece3df999d45561d
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2026-05-05 16:24:52 -0400

    kwidgetsaddons: Fix memory leaks

--- src/libraries/extras/kwidgetsaddons/main.c
+++ src/libraries/extras/kwidgetsaddons/main.c
@@ -3,17 +3,26 @@
 int main(int argc, char* argv[]) {
     QApplication* qapp = q_application_new(&argc, argv);
 
-    KGuiItem* primaryAction = k_guiitem_new7("Hello", "view-filter", "This is a tooltip", "This is a WhatsThis help text.");
+    KGuiItem* primary_item = k_guiitem_new7("Hello",
+                                            "view-filter",
+                                            "This is a tooltip",
+                                            "This is a WhatsThis help text.");
 
-    KGuiItem* secondaryAction = k_guiitem_new2("Bye");
+    KGuiItem* secondary_item = k_guiitem_new2("Bye");
 
-    int32_t res = k_messagebox_question_two_actions(NULL, "Description to tell you to click<br />on <b>either</b> button", "Qt 6 KMessageBox Example", primaryAction, secondaryAction, "", KMESSAGEBOX_OPTION_NOTIFY);
+    int32_t res = k_messagebox_question_two_actions(NULL,
+                                                    "Description to tell you to click<br />on <b>either</b> button",
+                                                    "Qt 6 KMessageBox Example",
+                                                    primary_item, secondary_item, "",
+                                                    KMESSAGEBOX_OPTION_NOTIFY);
 
     if (res == KMESSAGEBOX_BUTTONCODE_PRIMARYACTION)
         printf("You clicked Hello\n");
     else
         printf("You clicked Bye\n");
 
+    k_guiitem_delete(secondary_item);
+    k_guiitem_delete(primary_item);
     q_application_delete(qapp);
 
     return 0;
