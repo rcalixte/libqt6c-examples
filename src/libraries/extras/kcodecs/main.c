1a4355bd39655a4b02eeee6cf3b21d0c7b957fff
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2026-08-12 17:06:53 -0400

    kcodecs: Use direct parameter

--- src/libraries/extras/kcodecs/main.c
+++ src/libraries/extras/kcodecs/main.c
@@ -14,12 +14,11 @@ int main(int argc, char* argv[]) {
 
     // Ownership of these widgets will be transferred to the widget via the layout
     QVBoxLayout* vboxlayout = q_vboxlayout_new2();
-    QLabel* label = q_label_new3("Available Encodings:");
     QListWidget* listwidget = q_listwidget_new2();
 
     q_listwidget_add_items(listwidget, names);
 
-    q_vboxlayout_add_widget(vboxlayout, label);
+    q_vboxlayout_add_widget(vboxlayout, q_label_new3("Available Encodings:"));
     q_vboxlayout_add_widget(vboxlayout, listwidget);
     q_widget_set_layout(widget, vboxlayout);
 
