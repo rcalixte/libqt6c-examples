aa84f57edc5fa6043550397c4e18b8f7d6f84cb7
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2026-08-19 16:07:23 -0400

    wiggly: Minimize allocations

--- src/wiggly/main.c
+++ src/wiggly/main.c
@@ -18,80 +18,77 @@ typedef struct {
     char text[MAX_LENGTH];
     size_t step;
     QWidget* widget;
+    QFontMetrics* font_metrics;
+    QColor* color;
 } WigglyWidget;
 
-static WigglyWidget* wiggly = NULL;
+static WigglyWidget wiggly;
 
 void on_paint_event(void* self, void* event UNUSED) {
-    QFont* font = q_font_new();
-    QFontMetrics* font_metrics = q_fontmetrics_new(font);
+    int32_t x = (q_widget_width(self) - q_fontmetrics_horizontal_advance(wiggly.font_metrics, wiggly.text)) / 4;
+    int32_t y = (q_widget_height(self) + q_fontmetrics_ascent(wiggly.font_metrics) - q_fontmetrics_descent(wiggly.font_metrics)) / 2;
 
-    int32_t x = (q_widget_width(self) - q_fontmetrics_horizontal_advance(font_metrics, wiggly->text)) / 4;
-    int32_t y = (q_widget_height(self) + q_fontmetrics_ascent(font_metrics) - q_fontmetrics_descent(font_metrics)) / 2;
-
-    QColor* color = q_color_new3();
     QStylePainter* painter = q_stylepainter_new(self);
 
-    for (size_t i = 0; i < strlen(wiggly->text); i++) {
-        int32_t index = (wiggly->step + i) % sine_table.len;
-        q_color_set_hsv(color, (63 - index) * (sine_table.len / 4), 255, 191);
-        q_stylepainter_set_pen(painter, color);
-        char ch_str[2] = {wiggly->text[i], '\0'};
+    for (size_t i = 0; i < strlen(wiggly.text); i++) {
+        int32_t index = (wiggly.step + i) % sine_table.len;
+        q_color_set_hsv(wiggly.color, (63 - index) * (sine_table.len / 4), 255, 191);
+        q_stylepainter_set_pen(painter, wiggly.color);
+        char ch_str[2] = {wiggly.text[i], '\0'};
         q_stylepainter_draw_text3(painter,
                                   x,
-                                  y - (sine_table.data.ints[index] * q_fontmetrics_height(font_metrics) * 2) / 300,
+                                  y - (sine_table.data.ints[index] * q_fontmetrics_height(wiggly.font_metrics) * 2) / 300,
                                   ch_str);
-        x += q_fontmetrics_horizontal_advance(font_metrics, ch_str) * 3;
+        x += q_fontmetrics_horizontal_advance(wiggly.font_metrics, ch_str) * 3;
     }
 
     q_stylepainter_delete(painter);
-    q_color_delete(color);
-    q_fontmetrics_delete(font_metrics);
-    q_font_delete(font);
 }
 
 void on_timer_event(void* self, void* event) {
-    if (q_timerevent_timer_id(event) == q_basictimer_timer_id(wiggly->timer)) {
-        wiggly->step += 1;
+    if (q_timerevent_timer_id(event) == q_basictimer_timer_id(wiggly.timer)) {
+        wiggly.step += 1;
         q_widget_update(self);
     } else {
         q_widget_super_timer_event(self, event);
     }
 }
 
-WigglyWidget* initialize_wiggly_widget(const char* text) {
-    WigglyWidget* wiggly = malloc(sizeof(WigglyWidget));
-    wiggly->step = 0;
-    snprintf(wiggly->text, MAX_LENGTH, "%s", text);
+void initialize_wiggly_widget(const char* text) {
+    wiggly.step = 0;
+    snprintf(wiggly.text, MAX_LENGTH, "%s", text);
 
-    wiggly->widget = q_widget_new2();
-    q_widget_set_background_role(wiggly->widget, QPALETTE_COLORROLE_MIDLIGHT);
-    q_widget_set_auto_fill_background(wiggly->widget, true);
+    wiggly.widget = q_widget_new2();
+    q_widget_set_background_role(wiggly.widget, QPALETTE_COLORROLE_MIDLIGHT);
+    q_widget_set_auto_fill_background(wiggly.widget, true);
 
-    wiggly->timer = q_basictimer_new();
-    q_basictimer_start3(wiggly->timer, 60, wiggly->widget);
+    wiggly.timer = q_basictimer_new();
+    q_basictimer_start3(wiggly.timer, 60, wiggly.widget);
 
     QFont* font = q_font_new();
     q_font_set_point_size(font, q_font_point_size(font) + 25);
-    q_widget_set_font(wiggly->widget, font);
+    q_widget_set_font(wiggly.widget, font);
     q_font_delete(font);
 
-    q_widget_on_paint_event(wiggly->widget, on_paint_event);
-    q_widget_on_timer_event(wiggly->widget, on_timer_event);
+    q_widget_on_paint_event(wiggly.widget, on_paint_event);
+    q_widget_on_timer_event(wiggly.widget, on_timer_event);
 
-    return wiggly;
+    QFont* wiggly_font = q_font_new();
+    wiggly.font_metrics = q_fontmetrics_new(wiggly_font);
+    wiggly.color = q_color_new3();
+    q_font_delete(wiggly_font);
 }
 
-void cleanup_wiggly_widget(WigglyWidget* wiggly) {
-    q_basictimer_delete(wiggly->timer);
-    q_widget_delete_later(wiggly->widget);
-    free(wiggly);
-    wiggly = NULL;
+void cleanup_wiggly_widget() {
+    q_basictimer_delete(wiggly.timer);
+    q_color_delete(wiggly.color);
+    q_fontmetrics_delete(wiggly.font_metrics);
+    q_widget_delete_later(wiggly.widget);
 }
 
 void on_text_changed(void* self UNUSED, const char* text) {
-    snprintf(wiggly->text, MAX_LENGTH, "%s", text);
-    q_widget_update(wiggly->widget);
+    snprintf(wiggly.text, MAX_LENGTH, "%s", text);
+    q_widget_update(wiggly.widget);
 }
 
 int main(int argc, char* argv[]) {
@@ -101,21 +98,21 @@ int main(int argc, char* argv[]) {
     q_dialog_set_window_title(dialog, "Qt 6 Wiggly Text Example");
     q_dialog_resize(dialog, 500, 180);
 
-    wiggly = initialize_wiggly_widget(wiggly_text);
+    initialize_wiggly_widget(wiggly_text);
     QLineEdit* line_edit = q_lineedit_new2();
     q_lineedit_set_text(line_edit, wiggly_text);
     q_lineedit_set_max_length(line_edit, MAX_LENGTH - 1);
     q_lineedit_on_text_changed(line_edit, on_text_changed);
 
     QVBoxLayout* layout = q_vboxlayout_new(dialog);
-    q_vboxlayout_add_widget(layout, wiggly->widget);
+    q_vboxlayout_add_widget(layout, wiggly.widget);
     q_vboxlayout_add_widget(layout, line_edit);
 
     q_dialog_show(dialog);
 
     int result = q_application_exec();
 
-    cleanup_wiggly_widget(wiggly);
+    cleanup_wiggly_widget();
     q_dialog_delete(dialog);
     q_application_delete(qapp);
 
